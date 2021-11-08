cae20913a8923119b997b8148a0f3c052fc7b7c0
shamikabansal <71285046+shamikabansal@users.noreply.github.com>
2021-11-08 18:07:59 +0000

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {View,Text,StyleSheet,Image,TextInput,TouchableOpacity,Alert,KeyboardAvoidingView} from 'react-native';
+import * as firebase from 'firebase'
+export default class LoginScreen extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+          emailId : '',
+          password: ''
+        }
+      }
+    
+      login=async(email,password)=>{
+        if (email && password){
+          try{
+            const response = await firebase.auth().signInWithEmailAndPassword(email,password)
+            if(response){
+              this.props.navigation.navigate('Transaction')
+            }
+          }
+          catch(error){
+            switch (error.code) {
+              case 'auth/user-not-found':
+                Alert.alert("user dosen't exists")
+                console.log("doesn't exist")
+                break
+              case 'auth/invalid-email':
+                Alert.alert('incorrect email or password')
+                console.log('invaild')
+                break
+            }
+          }
+        }
+        else{
+            Alert.alert('enter email and password');
+        }
+      }
+
+  render(){
+      return(
+        <KeyboardAvoidingView style = {{alignItems:'center',marginTop:20}}>
+        <View>
+          <Image
+            source={require("../assets/booklogo.jpg")}
+            style={{width:200, height: 200}}/>
+          <Text style={{textAlign: 'center', fontSize: 30}}>Wily</Text>
+        </View>
+       
+      
+      </KeyboardAvoidingView>
+
+      )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  loginBox:
+  {
+    width: 300,
+  height: 40,
+  borderWidth: 1.5,
+  fontSize: 20,
+  margin:10,
+  paddingLeft:10
+  }
+})
